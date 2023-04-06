e5321530a75670d8a909c6734408dcde6c5c57f8
WilliamC111 <110690003+WilliamC111@users.noreply.github.com>
2023-04-06 09:44:21 -0500

    Delete src directory

--- src/view/Constants.java
+++
@@ -1,6 +0,0 @@
-package view;
-
-public class Constants {
-    public static final String MAIN_MENU = "Bienvenidos a [Hospital A.W]\n1. Crear una habitación\n2. Crear un paciente"
-            + "\n3. Mostrar historial de pacientes por habitación\n4. Generar XML\n5. Salir\n";
-}
