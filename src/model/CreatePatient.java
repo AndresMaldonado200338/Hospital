8b95f40e878ba4298216528c98fd574c37a589c4
Andres Maldonado <andres.maldonado02@uptc.edu.co>
2023-04-11 09:34:04 -0500

    view-patients final

--- src/model/CreatePatient.java
+++ src/model/CreatePatient.java
@@ -5,40 +5,43 @@ import java.util.List;
 import java.util.Scanner;
 
 public class CreatePatient {
-    public static void main(String[] args) {
-        Scanner console = new Scanner(System.in);
-        List<Patient> patients = new ArrayList<Patient>();
 
-        System.out.print("Ingrese pacientes: ");
-        int numPatients = console.nextInt();
-        console.nextLine();
+	public static void main(String[] args) {
+		Scanner console = new Scanner(System.in);
+		List<Patient> patients = new ArrayList<Patient>();
 
-        for (int i = 0; i < numPatients; i++) {
-            System.out.println("Ingresando los datos del paciente " + (i + 1));
-            System.out.print("Nombre de paciente: ");
-            String name = console.nextLine();
+		System.out.print("Ingrese el numero de pacientes: ");
+		int numPatients = console.nextInt();
+		console.nextLine();
 
-            System.out.print("Apellido del paciente: ");
-            String lastName = console.nextLine();
+		for (int i = 0; i < numPatients; i++) {
+			System.out.println("Ingresando los datos del paciente " + (i + 1));
+			System.out.print("Numero de telefono: ");
+			String contactPhoneNumber = console.nextLine();
 
-            System.out.print("Numero de contacto: ");
-            String contactNumber = console.nextLine();
+			System.out.print("Nombre: ");
+			String firstName = console.nextLine();
 
-            System.out.print("Estado: ");
-            Status status = Status.ACTIVE.getName();
-            console.nextLine();
+			System.out.print("Apellido: ");
+			String lastName = console.nextLine();
 
-            Patient patient = new Patient();
-            patient.setfirstName(name);
-            patient.setLastName(lastName);
-            patient.setContactPhoneNumber(Id);
-            patient.setStatus(status);
+			System.out.print("Estado: ACTIVO\n");
+			Status status = Status.ACTIVE;
 
-            rooms.add(room);
-            System.out.println("\nLista de habitaciones:");
-            for (Room room1 : rooms) {
-                System.out.println(room1.toString());
-            }
-        }
-    }
+			Patient patient = new Patient();
+			patient.setContactPhoneNumber(contactPhoneNumber);
+			patient.setFirstName(firstName);
+			patient.setLastName(lastName);
+			patient.setStatus(status);
+
+			patients.add(patient);
+		}
+
+		System.out.println("\nLista de pacientes:");
+		for (Patient patient : patients) {
+			System.out.println(
+					"Numero de Telefono: " + patient.getContactPhoneNumber() + " | Nombre: " + patient.getFirstName()
+							+ " | Apellido: " + patient.getLastName() + " | Estado: " + patient.getStatus().getName());
+		}
+	}
 }
